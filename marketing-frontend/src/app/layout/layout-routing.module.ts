210035d152fe943b13fa1ab0e956ebe6795e4ce4
Gariix1 <fasza212@gmail.com>
2022-07-22 19:26:06 -0500

    errores de merge

--- marketing-frontend/src/app/layout/layout-routing.module.ts
+++ marketing-frontend/src/app/layout/layout-routing.module.ts
@@ -1,35 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { CampaignComponent } from '../feature/campaign/form/campaign.component';
-=======
-import { CampaignComponent } from '../feature/campaign/campaign.component';
-<<<<<<< HEAD
-import { ProspectComponent } from '../feature/prospect/prospect.component';
-=======
->>>>>>> a9a664416dba71c2deb35801768b699f388dc3d3
-import { OportunidadComponent } from '../feature/oportunidad/oportunidad.component';
 import { ProspectComponent } from '../feature/prospect/form/prospect.component';
->>>>>>> 043f0a31ce2efc82b5ac3c1c01d91c4a8188c745
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainComponent } from './main/main.component';
 import { PersonComponent } from '../feature/person/form/person.component';
 import { PersonListComponent } from '../feature/person/list/person-list.component';
-<<<<<<< HEAD
+
 import { CampaignListComponent } from '../feature/campaign/list/campaign-list.component';
-=======
-<<<<<<< HEAD
+
 import { ProductosComponent } from '../feature/productos/form/productos.component';
 import { ProductosListComponent } from '../feature/productos/productos-list/productos-list.component';
-=======
-<<<<<<< HEAD
+
+
 import { ListComponent } from '../feature/oportunidad/list/list.component';
 import { OportunidadComponent } from '../feature/oportunidad/form/oportunidad.component';
-=======
->>>>>>> a9a664416dba71c2deb35801768b699f388dc3d3
+
+
 import { ProspectListComponent } from '../feature/prospect/prospect-list/prospect-list.component';
->>>>>>> 043f0a31ce2efc82b5ac3c1c01d91c4a8188c745
->>>>>>> f03c31a85e4f319d3abe55b36eddf70326ff00c0
+
 
 const routes: Routes = [
   {
@@ -53,15 +43,9 @@ const routes: Routes = [
 
       //campaign
       { path: 'campaign', component: CampaignComponent },
-<<<<<<< HEAD
       {path: 'campaign-list', component: CampaignListComponent},
   {path: 'campaign-form', component: CampaignComponent},
   {path: 'campaign-form/:id', component: CampaignComponent},
-=======
-      /*{path: 'campaign-list', component: CampaignListComponent},
-        {path: 'campaign-form', component: CampaignComponent},
-        {path: 'campaign-form/:id', component: CampaignComponent},*/
->>>>>>> a9a664416dba71c2deb35801768b699f388dc3d3
 
       //productos
       { path: 'productos', component: ProductosComponent },
